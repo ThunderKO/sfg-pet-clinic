b924b4e973131ac67a13596e3eb2517a56146d5a
ThunderKO <ko.ting.andy@gmail.com>
2020-01-11 21:39:46 +0800

    implement interfaces for services. Closes #5.

---
+++ pet-clinic-data/src/main/java/com/ione/sfgpetclinic/services/PetService.java
@@ -0,0 +1,15 @@
+package com.ione.sfgpetclinic.services;
+
+import com.ione.sfgpetclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
+
+}
