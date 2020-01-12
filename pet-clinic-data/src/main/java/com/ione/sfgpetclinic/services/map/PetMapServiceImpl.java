cca94a12b9b651b0baf3983753dea409eae7560c
ThunderKO <ko.ting.andy@gmail.com>
2020-01-12 09:37:09 +0800

    impl map based service. Closes #6.

---
+++ pet-clinic-data/src/main/java/com/ione/sfgpetclinic/services/map/PetMapServiceImpl.java
@@ -0,0 +1,33 @@
+package com.ione.sfgpetclinic.services.map;
+
+import com.ione.sfgpetclinic.model.Pet;
+import com.ione.sfgpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PetMapServiceImpl extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
