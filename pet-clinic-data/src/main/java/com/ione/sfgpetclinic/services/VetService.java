e8e7e1444747d40711dcbde0721b63e2c7733381
ThunderKO <ko.ting.andy@gmail.com>
2020-01-12 09:11:49 +0800

    refactoring to common base. Closes #11.

--- pet-clinic-data/src/main/java/com/ione/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/ione/sfgpetclinic/services/VetService.java
@@ -2,14 +2,6 @@ package com.ione.sfgpetclinic.services;
 
 import com.ione.sfgpetclinic.model.Vet;
 
-import java.util.Set;
-
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long> {
 
 }
