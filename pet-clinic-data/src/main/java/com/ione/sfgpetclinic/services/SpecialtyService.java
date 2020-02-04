2fc992c4f209430f28fd00f1eb1301244009c2d3
ThunderKO <ko.ting.andy@gmail.com>
2020-02-04 22:50:55 +0800

    Adding Specialty Map Service Impl. Closes #31.

---
+++ pet-clinic-data/src/main/java/com/ione/sfgpetclinic/services/SpecialtyService.java
@@ -0,0 +1,6 @@
+package com.ione.sfgpetclinic.services;
+
+import com.ione.sfgpetclinic.model.Speciality;
+
+public interface SpecialtyService extends CrudService<Speciality, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/ione/sfgpetclinic/services/map/SpecialtyMapServiceImpl.java
@@ -0,0 +1,35 @@
+package com.ione.sfgpetclinic.services.map;
+
+import com.ione.sfgpetclinic.model.Speciality;
+import com.ione.sfgpetclinic.services.SpecialtyService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialtyMapServiceImpl extends AbstractMapService<Speciality, Long> implements SpecialtyService {
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
