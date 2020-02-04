7f9cbe2f38a004c3ce8e063a4bdf919539ae7e1c
ThunderKO <ko.ting.andy@gmail.com>
2020-02-04 17:25:30 +0800

    Adding Pet Type, Pet, Visit Entity. Closes #26.

---
+++ pet-clinic-data/src/main/java/com/ione/sfgpetclinic/model/Visit.java
@@ -0,0 +1,33 @@
+package com.ione.sfgpetclinic.model;
+
+import java.time.LocalDate;
+
+public class Visit extends BaseEntity {
+    private LocalDate date;
+    private String descriptions;
+    private Pet pet;
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getDescriptions() {
+        return descriptions;
+    }
+
+    public void setDescriptions(String descriptions) {
+        this.descriptions = descriptions;
+    }
+
+    public Pet getPet() {
+        return pet;
+    }
+
+    public void setPet(Pet pet) {
+        this.pet = pet;
+    }
+}
