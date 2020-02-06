a56474246f60dcdf5caa0de0428f3b2410f152eb
ThunderKO <ko.ting.andy@gmail.com>
2020-02-06 18:41:59 +0800

    Added Visit Entity. Closes #38.

--- pet-clinic-data/src/main/java/com/ione/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/ione/sfgpetclinic/model/Pet.java
@@ -1,9 +1,9 @@
 package com.ione.sfgpetclinic.model;
 
-import javax.persistence.Column;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Pet extends BaseEntity {
 
@@ -20,6 +20,9 @@ public class Pet extends BaseEntity {
 
     @Column(name = "birth_date")
     private LocalDate birthDate;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
 
     public String getName() {
         return name;
@@ -51,5 +54,13 @@ public class Pet extends BaseEntity {
 
     public void setBirthDate(LocalDate birthDate) {
         this.birthDate = birthDate;
+    }
+
+    public Set<Visit> getVisits() {
+        return visits;
+    }
+
+    public void setVisits(Set<Visit> visits) {
+        this.visits = visits;
     }
 }
